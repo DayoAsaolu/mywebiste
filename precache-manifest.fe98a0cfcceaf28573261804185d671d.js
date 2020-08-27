280fd4c4030d13af89725f23cc02f259ce7002a8
David Asaolu <asaoludavid@gmail.com>
2020-08-27 00:01:08 -0230

    Updates

---
+++ precache-manifest.fe98a0cfcceaf28573261804185d671d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a88845bc8e3ddf68a65696f75c50459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "958476e2524630748e8f",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1863373b8213533a5ac2",
+    "url": "/static/css/main.d743f6ea.chunk.css"
+  },
+  {
+    "revision": "958476e2524630748e8f",
+    "url": "/static/js/2.562f626f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.562f626f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1863373b8213533a5ac2",
+    "url": "/static/js/main.a8dd5d11.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  },
+  {
+    "revision": "17e202b534044e2e77c9b49421716d22",
+    "url": "/static/media/index.17e202b5.png"
+  },
+  {
+    "revision": "819762ac5851256914fe5dd1f07ae4ea",
+    "url": "/static/media/web.819762ac.jpg"
+  },
+  {
+    "revision": "3808d279a6c3f4de9e6003f3ea75fdaa",
+    "url": "/static/media/web1.3808d279.jpg"
+  }
+]);
